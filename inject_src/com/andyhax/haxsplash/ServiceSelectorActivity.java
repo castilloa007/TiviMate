47d48c8d8136aa9aacddd3f5d7dce34894ecb60b
castilloa007 <castilloa007@users.noreply.github.com>
2026-05-05 21:57:15 -0500

    Show account picker dialog; call m1616Go directly to bypass Service form

--- inject_src/com/andyhax/haxsplash/ServiceSelectorActivity.java
+++ inject_src/com/andyhax/haxsplash/ServiceSelectorActivity.java
@@ -1,56 +1,42 @@
 package com.andyhax.haxsplash;
 
 import android.app.Activity;
-import android.content.ComponentName;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class ServiceSelectorActivity extends Activity {
 
-    private static final String[] PRESET_NAMES = {"Papai", "Mel", "Ivan"};
-    private static final String[] PRESET_HOST  = {
+    private static final String[] NAMES = {"Papai", "Mel", "Ivan"};
+    private static final String[] HOST  = {
         "http://ky-tv.cc:80", "http://ky-tv.cc:80", "http://ky-tv.cc:80"};
-    private static final String[] PRESET_USER  = {
+    private static final String[] USER  = {
         "AEGVXXZVZV", "Melli3B3llie@832", "icastil@1997"};
-    private static final String[] PRESET_PASS  = {
+    private static final String[] PASS  = {
         "236267373", "Fir3F@xed2020", "TheFireF@x3733"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        populatePortals();
-        launchMain();
+        showPicker();
     }
 
-    private static void populatePortals() {
-        AndyHax._portals.clear();
-        for (int i = 0; i < PRESET_NAMES.length; i++) {
-            PortalModel pm = new PortalModel();
-            pm.id_bClNU2OajLbxJWVW   = i + 1;
-            pm.name_doQhQ7J9PskxUJv5 = PRESET_NAMES[i];
-            pm.url_rH6MnarmmBvhjdPh  = buildXcUrl(PRESET_HOST[i], PRESET_USER[i], PRESET_PASS[i]);
-            AndyHax._portals.add(pm);
-        }
-        // Empty-URL "Custom" entry so TiviMate falls through to its manual form.
-        PortalModel custom = new PortalModel();
-        custom.id_bClNU2OajLbxJWVW   = PRESET_NAMES.length + 1;
-        custom.name_doQhQ7J9PskxUJv5 = "Custom...";
-        custom.url_rH6MnarmmBvhjdPh  = "";
-        AndyHax._portals.add(custom);
+    private void showPicker() {
+        new AlertDialog.Builder(this)
+            .setTitle("Select Account")
+            .setItems(NAMES, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String url = xc(HOST[which], USER[which], PASS[which]);
+                    AndyHax.m1616Go(url, ServiceSelectorActivity.this);
+                    finish();
+                }
+            })
+            .setCancelable(false)
+            .show();
     }
 
-    private static String buildXcUrl(String host, String user, String pass) {
-        return "xc:{\"h\":\"" + host + "\",\"u\":\"" + user
-             + "\",\"p\":\"" + pass + "\",\"o\":\"ts\"}";
-    }
-
-    private void launchMain() {
-        Intent intent = new Intent();
-        intent.setComponent(new ComponentName(
-            "ar.tvplayer.tv", "ar.tvplayer.tv.ui.MainActivity"));
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                      | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-        finish();
+    private static String xc(String h, String u, String p) {
+        return "xc:{\"h\":\"" + h + "\",\"u\":\"" + u + "\",\"p\":\"" + p + "\",\"o\":\"ts\"}";
     }
 }
