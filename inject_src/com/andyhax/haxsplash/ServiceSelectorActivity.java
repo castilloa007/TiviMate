c27d39fa66fa4d5c83b6e6f3e1dab0c6a0b818eb
castilloa007 <castilloa007@users.noreply.github.com>
2026-05-05 23:15:00 -0500

    Re-enable single manual portal option to avoid Service-only Cancel screen

--- inject_src/com/andyhax/haxsplash/ServiceSelectorActivity.java
+++ inject_src/com/andyhax/haxsplash/ServiceSelectorActivity.java
@@ -7,6 +7,19 @@ import android.os.Bundle;
 
 public class ServiceSelectorActivity extends Activity {
 
+    private static final String[] NAMES = {
+        "ky-tv.cc:80"
+    };
+    private static final String[] HOST  = {
+        "ky-tv.cc:80"
+    };
+    private static final String[] USER  = {
+        "username"
+    };
+    private static final String[] PASS  = {
+        "password"
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +28,20 @@ public class ServiceSelectorActivity extends Activity {
             "ar.tvplayer.tv", "ar.tvplayer.tv.ui.MainActivity"));
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                       | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        AndyHax._portals.clear();
+        for (int i = 0; i < NAMES.length; i++) {
+            PortalModel pm = new PortalModel();
+            pm.id_bClNU2OajLbxJWVW = i;
+            pm.name_doQhQ7J9PskxUJv5 = NAMES[i];
+            pm.url_rH6MnarmmBvhjdPh = xc(HOST[i], USER[i], PASS[i]);
+            AndyHax._portals.add(pm);
+        }
         startActivity(intent);
         finish();
+    }
+
+    private static String xc(String h, String u, String p) {
+        return "xc:{\"h\":\"" + h + "\",\"u\":\"" + u
+             + "\",\"p\":\"" + p + "\",\"o\":\"ts\"}";
     }
 }
