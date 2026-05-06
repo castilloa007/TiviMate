ef17c6fde6577035b46b9f8eec4ed9c87839e0c4
castilloa007 <castilloa007@users.noreply.github.com>
2026-05-05 22:50:57 -0500

    Add single Xtream Codes portal to avoid Service screen showing only Cancel

--- inject_src/com/andyhax/haxsplash/ServiceSelectorActivity.java
+++ inject_src/com/andyhax/haxsplash/ServiceSelectorActivity.java
@@ -7,6 +7,21 @@ import android.os.Bundle;
 
 public class ServiceSelectorActivity extends Activity {
 
+    // Keep at least one portal so the Service screen has selectable options.
+    // This opens TiviMate's Xtream form where user can type credentials.
+    private static final String[] NAMES = {
+        "Xtream Codes"
+    };
+    private static final String[] HOST  = {
+        ""
+    };
+    private static final String[] USER  = {
+        ""
+    };
+    private static final String[] PASS  = {
+        ""
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +30,20 @@ public class ServiceSelectorActivity extends Activity {
             "ar.tvplayer.tv", "ar.tvplayer.tv.ui.MainActivity"));
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                       | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        AndyHax._portals.clear();
+        for (int i = 0; i < NAMES.length; i++) {
+            PortalModel pm = new PortalModel();
+            pm.id_bClNU2OajLbxJWVW = i;
+            pm.name_doQhQ7J9PskxUJv5 = NAMES[i];
+            pm.url_rH6MnarmmBvhjdPh = xc(HOST[i], USER[i], PASS[i]);
+            AndyHax._portals.add(pm);
+        }
         startActivity(intent);
         finish();
+    }
+
+    private static String xc(String h, String u, String p) {
+        return "xc:{\"h\":\"" + h + "\",\"u\":\"" + u
+             + "\",\"p\":\"" + p + "\",\"o\":\"ts\"}";
     }
 }
